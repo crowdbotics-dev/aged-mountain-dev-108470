a730ff22857660910cd0f82b33f05a45068d9e33
Crowdbotics <engineering@crowdbotics.com>
2023-08-09 08:45:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-mountain-dev-108470",
+  url: "https://aged-mountain-dev-108470.botics.co",
+  api: "https://aged-mountain-dev-108470.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
